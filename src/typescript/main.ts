b910cda66f857059941831d11ff9ba2d214cf1ef
PxViet <repsweet080303@gmail.com>
2023-02-08 00:27:40 +0700

    #2 array and primitipe types

---
+++ src/typescript/main.ts
@@ -0,0 +1,10 @@
+const myArray : string[] = ['hi', 'sd'];
+const myArrayToo: Array<number> = [2,3,4,1];
+const myString : string = 'supermain';
+const myNumber : number = 99;
+const myBoolean : boolean = true;
+console.log(typeof myArray);
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(myArrayToo);
