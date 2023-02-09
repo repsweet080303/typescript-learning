2cfbd5334037218e140fe7a3609988c6a4bbc5cb
PxViet <repsweet080303@gmail.com>
2023-02-10 00:38:12 +0700

    #8 example literal type

---
+++ src/typescript/literalType.ts
@@ -0,0 +1,11 @@
+type Color = 'red' | 'green' | 'blue';
+
+const myColor: 'red' = 'red';
+
+// myColor = 'red' // constant can assign
+
+let yourColor: Color = 'blue';
+
+yourColor = 'red'
+
+console.log(myColor);
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,10 +1,1 @@
-function myFunction(name: string | string[]) {
-  if(Array.isArray(name)){
-    name.forEach(e => console.log(`they are ${e}`))
-  } else {
-    console.log(`there is ${name}`);
-  }
-}
-
-myFunction(['Hoa','Tien'])
-myFunction('Minh')
+import '../typescript/literalType'
